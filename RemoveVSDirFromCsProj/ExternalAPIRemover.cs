68b79e9afc1bd8738863faa44cdcd765d834113a
Josh Varty <joshvarty@gmail.com>
2017-07-11 15:26:55 -0700

    Change namespace

--- RemoveVSDirFromCsProj/ExternalAPIRemover.cs
+++ RemoveVSDirFromCsProj/ExternalAPIRemover.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
-namespace RemoveVSDirFromCsProj
+namespace RazzleRemover
 {
     /// <summary>
     /// This class is meant to remove ExternalAPI references like: 
