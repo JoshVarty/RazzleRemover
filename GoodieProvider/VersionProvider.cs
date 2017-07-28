98a6e54cd69c607754de19f159ee698cf0003cbb
Amadeusz Wieczorek <amwieczo@microsoft.com>
2017-07-28 08:46:41 -0700

    generates the versions.props file

--- GoodieProvider/VersionProvider.cs
+++ GoodieProvider/VersionProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Xml.Linq;
 
 namespace GoodieProvider
@@ -40,7 +41,7 @@ namespace GoodieProvider
                     continue;
                 }
                 PackageVersions.Add(name, version);
-                var propertyName = getPropertyNameForPackage(name);
+                var propertyName = getPropertyNameForPackage(name, declaration: false);
                 reference.Elements().Single(n => n.Name.LocalName == "Version").SetValue(propertyName);
             }
             // TODO: Save xe
@@ -50,20 +51,47 @@ namespace GoodieProvider
 
         private void UpdateAndSaveVersionProps(string sourcePath)
         {
+            var versionsPropsPath = Path.Combine(sourcePath, "build", "versions.props");
+            var directoryPath = Path.Combine(sourcePath, "build");
+
             // Load existing versions.props
-            var existingVersions = LoadProps(Path.Combine(sourcePath, "build", "versions.props"));
+            var existingVersions = LoadProps(versionsPropsPath);
             // Add newly discovered versions
             foreach (var discoveredVersion in PackageVersions)
             {
                 var name = discoveredVersion.Key;
-                var propertyName = getPropertyNameForPackage(name);
+                var propertyName = getPropertyNameForPackage(name, declaration: true);
                 foreach (var version in discoveredVersion.Value)
                 {
                     existingVersions.Add(propertyName, version);
                 }
             }
             // Save the combined versions.props
+            var sb = new StringBuilder();
+            sb.AppendLine($"<Project xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">");
+            sb.AppendLine($"  <PropertyGroup>");
+            foreach (var reference in existingVersions)
+            {
+                var name = reference.Key;
+                int valueCount = 0;
+                if (reference.Value.Count == 1)
+                {
+                    sb.AppendLine($"    <{name}>{reference.Value.Single()}</{name}>");
+                }
+                else
+                {
+                    foreach (var value in reference.Value)
+                    {
+                        Console.WriteLine($"Warning: Package {name} is referenced by multiple versions: {value}");
+                        sb.AppendLine($"    <{name}>{value}</{name}>");
+                    }
+                }
+            }
+            sb.AppendLine($"  </PropertyGroup>");
+            sb.AppendLine($"</Project>");
 
+            if (!Directory.Exists(directoryPath)) Directory.CreateDirectory(directoryPath);
+            File.WriteAllText(versionsPropsPath, sb.ToString());
         }
 
         private MultiValueDictionary<string, string> LoadProps(string propsPath)
@@ -80,10 +108,10 @@ namespace GoodieProvider
         /// Creates a MSBuild property for a given package name.
         /// Removes dots from the given package name.
         /// </summary>
-        private string getPropertyNameForPackage(string name)
+        private string getPropertyNameForPackage(string name, bool declaration)
         {
             var processedName = name.Replace(".", "");
-            return $"$({processedName})";
+            return declaration ? processedName : $"$({processedName})";
         }
     }
 }
